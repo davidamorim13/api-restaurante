b8bdc387e67bd06762544815035336e5b4303e11
davidamorim13 <dr4714@gmail.com>
2024-06-08 18:37:51 -0300

    Adicionado status de retorno em cada verbo

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -10,7 +10,7 @@ import {
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('categories')
 
@@ -19,21 +19,34 @@ export class CategoriesController {
 	constructor(private readonly categoriesService: CategoriesService) {}
 
 	@Post()
+	@ApiResponse({status:201, description:"A categoria foi cadastrada com sucesso."})
+	@ApiResponse({status:400, description: "O correu um erro ao cadastrar."})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	create(@Body() createCategoryDto: CreateCategoryDto) {
 		return this.categoriesService.create(createCategoryDto);
 	}
 
 	@Get()
+	@ApiResponse({status:200, description:"Leitura com sucesso."})
+	@ApiResponse({status:404, description: "Categoria não encontrada"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	findAll() {
 		return this.categoriesService.findAll();
 	}
 
 	@Get(':id')
+	@ApiResponse({status:200, description:"Categoria encontrada com sucesso."})
+	@ApiResponse({status:404, description: "Categoria não encontrada"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	findOne(@Param('id') id: string) {
 		return this.categoriesService.findOne(+id);
 	}
 
 	@Patch(':id')
+	@ApiResponse({status:200, description:"Categoria atualizada com sucesso."})
+	@ApiResponse({status:400, description: "O correu um erro ao atualizar a categoria."})
+	@ApiResponse({status:404, description: "Categoria não encontrado"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	update(
 		@Param('id') id: string,
 		@Body() updateCategoryDto: UpdateCategoryDto,
@@ -42,6 +55,9 @@ export class CategoriesController {
 	}
 
 	@Delete(':id')
+	@ApiResponse({status:200, description:"Categoria excluida com sucesso."})
+	@ApiResponse({status:404, description: "Categoria não encontrada"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	remove(@Param('id') id: string) {
 		return this.categoriesService.remove(+id);
 	}
--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -10,7 +10,7 @@ import {
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
-import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('customers')
 
@@ -19,25 +19,35 @@ export class CustomersController {
 	constructor(private readonly customersService: CustomersService) {}
 
 	@Post()
-	@ApiResponse({status:201, description:"O produto foi criado com sucesso."})
+	@ApiResponse({status:201, description:"Cliente cadastrado com sucesso."})
 	@ApiResponse({status:400, description: "O correu um erro ao cadastrar um produto."})
-	@ApiResponse({status:404, description: "Produto não encontrado."})
-	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
+	@ApiResponse({status:404, description: "Cliente não encontrado."})
+	@ApiResponse({status:500, description: "O correu um erro interno no servidor."})
 	create(@Body() createCustomerDto: CreateCustomerDto) {
 		return this.customersService.create(createCustomerDto);
 	}
 
 	@Get()
+	@ApiResponse({status:200, description:"Leitura com sucesso."})
+	@ApiResponse({status:404, description: "Cliente não encontrada"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	findAll() {
 		return this.customersService.findAll();
 	}
 
 	@Get(':id')
+	@ApiResponse({status:200, description:"Cliente encontrado com sucesso."})
+	@ApiResponse({status:404, description: "Cliente não encontrado"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	findOne(@Param('id') id: string) {
 		return this.customersService.findOne(+id);
 	}
 
 	@Patch(':id')
+	@ApiResponse({status:200, description:"Cadastro do cliente atualizado com sucesso."})
+	@ApiResponse({status:400, description: "O correu um erro ao atualizar o cadastro do cliente."})
+	@ApiResponse({status:404, description: "Cliente não encontrado"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	update(
 		@Param('id') id: string,
 		@Body() updateCustomerDto: UpdateCustomerDto,
@@ -46,6 +56,9 @@ export class CustomersController {
 	}
 
 	@Delete(':id')
+	@ApiResponse({status:200, description:"Cadastro de cliente excluido com sucesso."})
+	@ApiResponse({status:404, description: "Cliente não encontrado"})
+	@ApiResponse({status:500, description: "Ocorreu um erro interno no servidor."})
 	remove(@Param('id') id: string) {
 		return this.customersService.remove(+id);
 	}
